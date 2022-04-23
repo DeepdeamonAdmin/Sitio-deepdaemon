28671990f4ad1f70b8878aaaf35af734cb151886
omarmorle <51431205+omarmorle@users.noreply.github.com>
2022-04-23 00:00:02 -0500

    se modifico para el ver mas

--- src/componentes/users/Projects.js
+++ src/componentes/users/Projects.js
@@ -2,11 +2,40 @@ import React from 'react';
 import './users.css'
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { deleteProject } from '../../actions/delete';
 
 export const Projects = () => {
 
+    const dispatch = useDispatch();
+
     const { projects } = useSelector( state => state.projects );
-            
+
+    let mostrador = 0;
+
+    const handleEnvProyect = (id) => {
+        //Imprimir el id
+        console.log(id);
+        dispatch(deleteProject(id));
+    }
+
+    const handleVermas = (id) => {
+        //console.log(id);
+        if(mostrador === 0){
+            //Poner visible el div con el id del proyecto
+            document.getElementById(id).style.display = 'block';
+            //Cambiar texto del boton
+            document.getElementById(id+"btn").innerHTML = 'Ver menos';
+            mostrador = 1;
+        }else{
+            //Poner invisible el div con el id del proyecto
+            document.getElementById(id).style.display = 'none';
+            //Cambiar texto del boton
+            document.getElementById(id+"btn").innerHTML = 'Ver más';
+            mostrador = 0;
+        }
+        
+    }
 
     return (
     <div className='container'>
@@ -54,19 +83,49 @@ export const Projects = () => {
                             </td>
                             <td className='td-project'>
                                 <button 
-                                className='btn-project-delete'
+                                    className='btn-project-delete'
+                                    onClick={ () => handleEnvProyect(project.id) }
                                 // onClick={ console.log('hola'); } 
                                 >
                                     Eliminar
                                 </button>   
                             </td>
                             <td className='td-project'>
+
+                                <button
+                                    //id del proyecto concatenado con el nombre del proyecto
+                                    id={project.id + "btn"}
+                                    className="btn btn-outline-secondary nav-item nav-link" 
+                                    onClick={ () => handleVermas(project.id) }>
+                                    Ver más
+                                </button>
+
+                                {/*
                                 <Link
-                                    to ={`/user/moreproject:${project.id}`}
+                                    to ={`/user/moreproject/${project.id}`}
                                     className="btn btn-outline-secondary nav-item nav-link"
                                 >
                                     Ver más
                                 </Link>
+                                */}
+                                <div id={project.id} style={{display: 'none'}}>
+                                    <div className='div-project'>
+                                        <div className='div-project-img'>
+                                            <img src={ project.urlImg }
+                                                className='foto-project'
+                                                alt="foto-perfil" />
+                                        </div>
+                                        <div className='div-project-info' witdh="100px">
+                                            <label className='label-project'>Nombre: {project.name}</label>
+                                            <br></br>
+                                            <label className='label-project'>Tecnología: {project.nameTech}</label>
+                                            <br></br>
+                                            <label className='label-project'>Enlace: {project.link}</label>
+                                            <br></br>
+                                            <label className='label-project'>Descripción: {project.descripcion}</label>
+                                        </div>
+                                    </div>
+                                </div>
                             </td>
                         </tr>
                     ))       
