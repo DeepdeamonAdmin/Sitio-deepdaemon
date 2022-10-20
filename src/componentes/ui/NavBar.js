1ab61077308ad513127200ae732a8f4c31032149
Gabriel Arzate <gabopuma289@gmail.com>
2022-10-19 21:55:15 -0500

    Se modifico la navbar para dezplegar el menu response

--- src/componentes/ui/NavBar.js
+++ src/componentes/ui/NavBar.js
@@ -1,42 +1,57 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 import { Link } from "react-scroll";
 
 import logo from "../../styles/assets/img/sitio/deepdaemon.png";
 
 export const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark" id="navbar">
+      <NavLink to="/">
+        <img src={logo} style={{ width: "30px" }} alt="logo" />
+      </NavLink>
+      <NavLink to="/" className="navbar-brand mb-0">
+        {" "}
+        Lab. CCC{" "}
+      </NavLink>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="true"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <Link
+            activeClass="active"
+            className="nav-item nav-link"
+            to="Nosotros"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            Nosotros
+          </Link>
 
+          <Link
+            activeClass="active"
+            className="nav-item nav-link"
+            to="Proyectos"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            Proyectos
+          </Link>
 
-	return (
-		<nav className="navbar navbar-expand-sm navbar-dark bg-dark" id="navbar">
-			<NavLink to="/">
-				<img src={logo} style={{ width: "30px" }} alt="logo" />
-			</NavLink>
-			<NavLink to="/" className="navbar-brand mb-0"> Lab. CCC </NavLink>
-
-			<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-				<div className="navbar-nav">
-					<Link activeClass="active"
-						className="nav-item nav-link"
-						to='Nosotros'
-						spy={true}
-						smooth={true}
-						offset={-70}
-						duration={500}>
-						Nosotros
-					</Link>
-
-					<Link activeClass="active"
-                        className="nav-item nav-link"
-                        to='Proyectos'
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}>
-                            Proyectos
-                    </Link>
-
-					{/* <Link activeClass="active"
+          {/* <Link activeClass="active"
 						className="nav-item nav-link"
 						to='Publicaciones'
 						spy={true}
@@ -46,55 +61,59 @@ export const NavBar = () => {
 						Publicaciones
 					</Link> */}
 
-					<Link activeClass="active"
-						className="nav-item nav-link"
-						to='Lideres'
-						spy={true}
-						smooth={true}
-						offset={-70}
-						duration={500}>
-						Lideres
-					</Link>
+          <Link
+            activeClass="active"
+            className="nav-item nav-link"
+            to="Lideres"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            Lideres
+          </Link>
 
-					<Link activeClass="active"
-						className="nav-item nav-link"
-						to='Equipo'
-						spy={true}
-						smooth={true}
-						offset={-70}
-						duration={500}>
-						Equipo
-					</Link>
+          <Link
+            activeClass="active"
+            className="nav-item nav-link"
+            to="Equipo"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            Equipo
+          </Link>
 
-					<Link activeClass="active"
-						className="nav-item nav-link"
-						to='Contacto'
-						spy={true}
-						smooth={true}
-						offset={-70}
-						duration={500}>
-						Contacto
-					</Link>
-				</div>
-				<div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-					<ul className="navbar-nav ml-auto">
-						<NavLink
-							to='/registrer'
-							className="btn btn-outline-secondary nav-item nav-link"
-
-						>
-							Registrar
-						</NavLink>
-						<NavLink
-							to='/login'
-							className="btn btn-outline-secondary nav-item nav-link"
-
-						>
-							Iniciar Sesión
-						</NavLink>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	)
-}
+          <Link
+            activeClass="active"
+            className="nav-item nav-link"
+            to="Contacto"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            Contacto
+          </Link>
+        </div>
+        <div className="navbar-collapse">
+          <ul className="navbar-nav ml-auto">
+            <NavLink
+              to="/registrer"
+              className="btn btn-outline-secondary nav-item nav-link"
+            >
+              Registrar
+            </NavLink>
+            <NavLink
+              to="/login"
+              className="btn btn-outline-secondary nav-item nav-link"
+            >
+              Iniciar Sesión
+            </NavLink>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
