e57f8507ddc7874ffe2d100abf834ffb9033ce7e
saulgame2010 <saulgame2010@gmail.com>
2023-03-23 20:51:45 -0600

    Se arregló la parte de listar las tesis de las cuales el usuario es autor

--- src/componentes/users/TesisListUser.js
+++ src/componentes/users/TesisListUser.js
@@ -12,9 +12,16 @@ const TesisListUser = () => {
     const currentUser = auth.currentUser.displayName;
 
     const { tesis } = useSelector(state => state.tesis);
+    /**
+     * * La comprobación se hace porque en el objeto tesis hay una propiedad llamada 'autores',
+     * * esa propiedad es un arreglo de strings pero en caso de que alguna tesis no tenga autores
+     * * el valor por defecto de esa propiedad será 'Sin autores', es decir, un string, no un arreglo,
+     * * por lo que al momento de iterar las tesis debemos comprobar de que sean un arreglo, ya que el
+     * * método some solo es para arreglos y si no hicieramos esa comprobación daría un error
+    */
     // Esta variable es para obtener solo las tesis que son del usuario
     const tesisDelUsuario = tesis.filter(tesis => {
-        if (!Array.isArray(tesis.autores)) {
+        if (!Array.isArray(tesis.autores)) { // Leer comentario de arriba para entender esta comprobación
             return false;
         }
         return tesis.autores.some(autor => autor.nombreAutor === currentUser);
@@ -23,8 +30,6 @@ const TesisListUser = () => {
 
     return (
         <>
-            {/* { loading && <p className="animate__animated animate__flash">Loading</p> } */}
-
             <div className="card-columns animate__animated animate__fadeIn">
                 {
                     // Aquí iteramos solo las tesis del usuario en lugar de todas las tesis
