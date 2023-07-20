6ddb9a4b0e0ab0666507b8a662a881a6bae64edf
emillie perez <eflores-11@hotmail.com>
2023-07-20 00:09:13 -0600

    Se corrigieron las cards de publicaciones, ya no hay espacios vacios ni desface de tamaños

--- src/componentes/Usuario/PublicationScreen.js
+++ src/componentes/Usuario/PublicationScreen.js
@@ -1,80 +1,71 @@
-import React from 'react'
-import {db} from '../../firebase/firebase-config';
-import { collection, getDocs, where, get, query } from "firebase/firestore";
-import { PublicationDetaills } from './PublicationDetaills';
-import { VerMas } from '../ui/VerMas';
-import { ModalCrearCuenta } from './ModalCrearCuenta';
+import React from 'react';
+import { db } from '../../firebase/firebase-config';
+import { collection, getDocs, where, query } from "firebase/firestore";
 import { VerMasPublication } from '../ui/VerMasPublication';
+import { ModalCrearCuenta } from './ModalCrearCuenta';
 
-export const PublicationScreen = ({type}) => {
- //const { projectsAll } = useSelector(state => state.publications);
-	//console.log(projectsAll)
-	const [publications, setPublications] = React.useState([])
-	React.useEffect(() => {
-		const getPublications = async () => {
-			try {
-                const ref = collection(db, "Publicaciones")
-				const q = query(ref, where("postType", '==', type))
-                //console.log(q)
-                const Data = await getDocs(q);
-				const arrayData = Data.docs.map(doc => ({id: doc.id, ...doc.data()}))
-				setPublications(arrayData)
-				
-			} catch (error) {
-				console.log(error)
-			}
-		}
-		getPublications()
-	}, [])
-	//console.log(publications)
-	if (publications.length === 0) {
-		return <p className="team_title">No hay publicaciones disponibles por el momento.</p>;
-	}
-	return (
-        <div className="container-fluid">
-            {publications.length === 0 && <p>No se encontraron publicaciones por el momento.</p>}
-            <div className="row">
-                {publications.map((publication, index) => (
-                    // Imprimir solamente si el estado es igual al seleccionado
-                    publication.display === "Yes" && (
-                        <div key={publication.id} className="col-md-4 mb-3">
-                            <div className="card h-100" style={{ margin: 0 }}>
-                                <div className="row g-0">
-                                    <div className="col-md-3">
-                                        <img
-                                            className="card-img"
-                                            src={publication.urlImg}
-                                            style={{
-                                                objectFit: 'cover',
-                                                objectPosition: 'center',
-                                                height: "100%",
-                                                width: '100%',
-                                                maxHeight: "90px",
-                                                maxWidth: '90px'
-                                            }}
-                                            alt="member"
-                                        />
-                                    </div>
-                                    <div className="col-md-6">
-                                        <div className="card-body text-dark d-flex flex-column h-100">
-                                            <h6 className="card-title" style={{ display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical', overflow: 'hidden', textOverflow: 'ellipsis' }}>
-                                                {publication.title}
-                                            </h6>
-                                            <div className="text-right">
-                                                
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-3 text-right">
-                                                <ModalCrearCuenta />
-                                                <VerMasPublication publicacion={publication} />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                ))}
+export const PublicationScreen = ({ type }) => {
+  const [publications, setPublications] = React.useState([]);
+
+  React.useEffect(() => {
+    const getPublications = async () => {
+      try {
+        const ref = collection(db, "Publicaciones");
+        const q = query(ref, where("postType", '==', type));
+        const Data = await getDocs(q);
+        const arrayData = Data.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        setPublications(arrayData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPublications();
+  }, [type]);
+
+  if (publications.length === 0) {
+    return <p className="team_title">No hay publicaciones disponibles por el momento.</p>;
+  }
+
+return (
+  <div className="container">
+    <div className="row">
+      {publications.map(publication => {
+        return (
+          publication.display === "Yes" && (
+            <div className="col-md-4" key={publication.id}>
+              <div className="d-flex flex-row card animate__animated animate__fadeIn border-primary mb-3" style={{ maxWidth: 360, height: 170, position: 'relative' }}>
+                <img
+                  className="card-img mt-3 ml-2"
+                  src={publication.urlImg}
+                  style={{
+                    objectFit: 'cover',
+                    objectPosition: 'center',
+                    height: "110px",
+                    width: '110px'
+                  }}
+                  alt="member"
+                />
+                <div className="card-body text-dark">
+                  {/* Limitar el título a tres líneas con puntos suspensivos */}
+                  <h6 className="card-title" style={{
+                    display: '-webkit-box',
+                    WebkitLineClamp: 3,
+                    WebkitBoxOrient: 'vertical',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    maxHeight: '3.6em',
+                  }}>
+                    {publication.title}
+                  </h6>
+                  <ModalCrearCuenta />
+                  <VerMasPublication publicacion={publication} />
+                </div>
+              </div>
             </div>
-        </div>
-    );
-}
+          )
+        );
+      })}
+    </div>
+  </div>
+);
+};
